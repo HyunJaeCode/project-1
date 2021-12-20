a16b38c1a414c6ee88146b73f37d935ee24971f7
Whale Belly <namro7633@gmail.com>
2021-12-20 13:45:32 +0900

    깃허브 처음 올리기

---
+++ src/HelloWorld/Byte.java
@@ -0,0 +1,18 @@
+package HelloWorld;
+
+public class Byte {
+
+	public static void main(String[] args) {
+		final int MAN_NUM = 100;
+		final float PI = 3.14f;
+		
+		long lNum = 10;
+		float fNum = lNum;
+		
+		System.out.println(fNum);
+	
+		
+		}
+		
+	}
+
---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		byte bData = -128;
+		System.out.println(bData);
+		
+		
+
+	}
+
+}
